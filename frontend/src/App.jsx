ca9422be9f39555398043a513e5ba9a7a83fe741
dainguyenn <dainguyenend@gmail.com>
2023-03-07 22:20:11 +0700

    Init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {UserIcon} from '@heroicons/react/24/solid'
+
+function App() { 
+  return (
+    <div className="bg-purple-50">
+       abc
+       <UserIcon className="w-4"/>
+    </div>
+  )
+}
+
+export default App
